715d584af0e683fde8b809819352ed3aa08b711c
vyshukavya <vyshukavya1029@gmail.com>
2026-07-04 15:43:25 +0530

    Module 1: GCD of Array

---
+++ module1/06_GCD_of_Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        small = min(nums)
+        large = max(nums)
+        for i in range(small, 0, -1):
+            if small % i == 0 and large % i == 0:
+                return i
